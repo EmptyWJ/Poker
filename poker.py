3ee3e3b9d6538a128a3037ea259c64b41dbf6898
EmptyWJ <1030685104@qq.com>
2020-03-12 20:32:18 +0800

    game code

--- poker.py
+++ poker.py
@@ -1,6 +1,6 @@
 import unittest
 
-class FizzBuzzTest(unittest.TestCase):
+class PokerGameTest(unittest.TestCase):
   def test_black_win(self):
     assert game("Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S") =="Black wins"
     assert game("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH") =="Black wins"
